cac689c5f1af1b2a09f4cf8a54433141919f0afe
QuyTVM <qtranvuminh@gmail.com>
2026-02-22 12:58:34 +0700

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+﻿export const events = [
+    {
+        image: "/images/event1.png",
+        title: "Global Tech Summit 2026",
+        slug: "global-tech-summit-2026",
+        location: "San Francisco, CA",
+        date: "March 15, 2026",
+        time: "09:00 AM"
+    },
+    {
+        image: "/images/event2.png",
+        title: "AI & Future Hackathon",
+        slug: "ai-future-hackathon",
+        location: "New York City, NY",
+        date: "April 22, 2026",
+        time: "10:00 AM"
+    },
+    {
+        image: "/images/event3.png",
+        title: "Frontend Masters Conference",
+        slug: "frontend-masters-conference",
+        location: "London, UK",
+        date: "May 10, 2026",
+        time: "08:30 AM"
+    },
+    {
+        image: "/images/event4.png",
+        title: "Blockchain Developers Meetup",
+        slug: "blockchain-developers-meetup",
+        location: "Berlin, DE",
+        date: "June 05, 2026",
+        time: "06:00 PM"
+    },
+    {
+        image: "/images/event5.png",
+        title: "Cloud Infrastructure Expo",
+        slug: "cloud-infrastructure-expo",
+        location: "Tokyo, JP",
+        date: "July 12, 2026",
+        time: "11:00 AM"
+    },
+    {
+        image: "/images/event6.png",
+        title: "Cyber Security Workshop",
+        slug: "cyber-security-workshop",
+        location: "Toronto, CA",
+        date: "August 20, 2026",
+        time: "02:00 PM"
+    }
+];
